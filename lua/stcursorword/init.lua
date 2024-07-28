83925cb09f8eb38e581e96c7f28616edccfab413
Tran Vo Son Tung <sontungexpt@gmail.com>
2024-07-28 18:04:34 +0700

    add terminal to excluded buftypes by default and change some syntax

--- lua/stcursorword/init.lua
+++ lua/stcursorword/init.lua
@@ -1,13 +1,12 @@
 local vim = vim
 local w, fn, api = vim.w, vim.fn, vim.api
-local hl, autocmd, get_cursor, get_line, matchstrpos, matchadd, strdisplaywidth =
+local hl, autocmd, get_line, get_cursor, matchstrpos, matchadd =
 	api.nvim_set_hl,
 	api.nvim_create_autocmd,
-	api.nvim_win_get_cursor,
 	api.nvim_get_current_line,
+	api.nvim_win_get_cursor,
 	fn.matchstrpos,
-	fn.matchadd,
-	fn.strdisplaywidth
+	fn.matchadd
 
 local PLUG_NAME = "stcursorword"
 local enabled = false
@@ -24,7 +23,7 @@ local default_configs = {
 		filetypes = {},
 		buftypes = {
 			"prompt",
-			-- "terminal",
+			"terminal",
 			-- "nofile",
 		},
 		patterns = {
@@ -48,7 +47,24 @@ local default_configs = {
 	},
 }
 
-local function matchdelete()
+local function merge_config(default_opts, user_opts)
+	local default_options_type = type(default_opts)
+
+	if default_options_type == type(user_opts) then
+		if default_options_type == "table" and default_opts[1] == nil then
+			for k, v in pairs(user_opts) do
+				default_opts[k] = merge_config(default_opts[k], v)
+			end
+		else
+			default_opts = user_opts
+		end
+	elseif default_opts == nil then
+		default_opts = user_opts
+	end
+	return default_opts
+end
+
+local matchdelete = function()
 	if w.stcursorword ~= nil then
 		pcall(fn.matchdelete, w.stcursorword)
 		w.stcursorword = nil
@@ -57,15 +73,16 @@ local function matchdelete()
 	end
 end
 
-local function highlight_same(configs)
+local highlight_same = function(configs)
+	if not enabled then return end
+
 	local cursor_pos = get_cursor(0)
 	local cursor_column = cursor_pos[2]
 	local cursor_line = cursor_pos[1]
 
 	-- if cusor doesn't move out of the word, do nothing
 	if
-		enabled
-		and prev_line == cursor_line
+		prev_line == cursor_line
 		and cursor_column >= prev_start_column
 		and cursor_column < prev_end_column
 	then
@@ -81,16 +98,18 @@ local function highlight_same(configs)
 	-- Fixes vim:E976 error when cursor is on a blob
 	if fn.type(line) == vim.v.t_blob then return end
 
+	-- get the left part of the word containing the cursor
 	local matches = matchstrpos(line:sub(1, cursor_column + 1), [[\w*$]])
 	local word = matches[1] -- left part of the word
 
 	if word ~= "" then
+		prev_start_column = matches[2]
+		-- get the right part of the word not containing the cursor
 		matches = matchstrpos(line, [[^\w*]], cursor_column + 1)
 		word = word .. matches[1] -- combine with right part of the word
-		prev_start_column = matches[2]
 		prev_end_column = matches[3]
 
-		local word_len = strdisplaywidth(word)
+		local word_len = #word
 		if word_len < configs.min_word_length or word_len > configs.max_word_length then return end
 
 		w.stcursorword =
@@ -98,33 +117,34 @@ local function highlight_same(configs)
 	end
 end
 
-local function arr_contains(tbl, value)
+local arr_contains = function(tbl, value)
 	for _, v in ipairs(tbl) do
 		if v == value then return true end
 	end
 	return false
 end
 
-local function matches_file_patterns(file_name, file_patterns)
+local matches_file_patterns = function(file_name, file_patterns)
 	for _, pattern in ipairs(file_patterns) do
 		if file_name:match(pattern) then return true end
 	end
 	return false
 end
 
-local function is_disabled(excluded, bufnr)
+local check_disabled = function(excluded, bufnr)
 	return arr_contains(excluded.buftypes, api.nvim_get_option_value("buftype", { buf = bufnr or 0 }))
 		or arr_contains(excluded.filetypes, api.nvim_get_option_value("filetype", { buf = bufnr or 0 }))
 		or matches_file_patterns(api.nvim_buf_get_name(bufnr or 0), excluded.patterns)
 end
 
-local function enable(configs)
+local enable = function(configs)
+	enabled = true
 	-- initial when plugin is loaded
-	hl(0, PLUG_NAME, configs.highlight)
 	local group = api.nvim_create_augroup(PLUG_NAME, { clear = true })
+	hl(0, PLUG_NAME, configs.highlight)
 
-	local disabled_buffer = is_disabled(configs.excluded, 0)
-	if not disabled_buffer then highlight_same(configs) end -- initial match
+	local disabled = check_disabled(configs.excluded, 0)
+	if not disabled then highlight_same(configs) end -- initial match
 
 	autocmd("ColorScheme", {
 		group = group,
@@ -143,8 +163,8 @@ local function enable(configs)
 			-- - The file type (filetype) is nil.
 			skip_cursormoved = true
 			vim.defer_fn(function()
-				disabled_buffer = is_disabled(configs.excluded, 0)
-				if not disabled_buffer then highlight_same(configs) end
+				disabled = check_disabled(configs.excluded, 0)
+				if not disabled then highlight_same(configs) end
 			end, 8)
 		end,
 	})
@@ -154,7 +174,7 @@ local function enable(configs)
 		callback = function()
 			if skip_cursormoved then
 				skip_cursormoved = false
-			elseif not disabled_buffer then
+			elseif not disabled then
 				highlight_same(configs)
 			end
 		end,
@@ -164,17 +184,15 @@ local function enable(configs)
 		group = group,
 		callback = matchdelete,
 	})
-
-	enabled = true
 end
 
-local function disable()
+local disable = function()
 	matchdelete()
 	api.nvim_del_augroup_by_name(PLUG_NAME)
 	enabled = false
 end
 
-local function toggle(configs)
+local toggle = function(configs)
 	if enabled then
 		disable()
 	else
@@ -182,24 +200,7 @@ local function toggle(configs)
 	end
 end
 
-local function merge_config(default_opts, user_opts)
-	local default_options_type = type(default_opts)
-
-	if default_options_type == type(user_opts) then
-		if default_options_type == "table" and default_opts[1] == nil then
-			for k, v in pairs(user_opts) do
-				default_opts[k] = merge_config(default_opts[k], v)
-			end
-		else
-			default_opts = user_opts
-		end
-	elseif default_opts == nil then
-		default_opts = user_opts
-	end
-	return default_opts
-end
-
-function M.setup(user_opts)
+M.setup = function(user_opts)
 	local opts = merge_config(default_configs, user_opts)
 	api.nvim_create_user_command("Cursorword", function(args)
 		local arg = string.lower(args.args)
